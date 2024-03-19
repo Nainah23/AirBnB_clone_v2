6c33797592ef3e4ad63105de91a94f828313fbbf
Issmuth <Ismael.mdev@gmail.com>
2024-03-19 13:34:34 +0300

    done

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -7,7 +7,7 @@ CREATE USER IF NOT EXISTS
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
 -- Grant all privileges to the created database
-GRANT * ON hbnb_dev_db	TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
 -- Grant select privileges to the user on the performance schema database
-GRANT SELECT ON performance_schema TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
