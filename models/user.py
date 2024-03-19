0a0359689fb33d1af7643cc9335ba63bb6a0cf54
Nainah23 <kamauwainaina29@gmail.com>
2024-03-19 13:07:09 -0700

    Update user

--- models/user.py
+++ models/user.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
+"""This module defines a class User
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+"""
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import BaseModel
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from models.place import Place
+from models.review import Review
 
 class User(BaseModel):
     """This class defines a user by various attributes"""
