865fae414e807c4cd8354616bde04299dbe1f5fa
Nainah23 <kamauwainaina29@gmail.com>
2024-03-20 06:59:15 -0700

    Committing changes before merge

--- models/place.py
+++ models/place.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String
-from sqlalchemy import Float, ForeignKey
+from sqlalchemy import Column, Integer, String, Float, ForeignKey, Table
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from os import getenv
+import models
 
+place_amenity = Table("place_amenity", Base.metadata, Column("place_id", String(60),
+    ForeignKey("places.id"), primary_key=True, nullable=False),
+    Column("amenity_id", String(60), ForeignKey("amenities.id"), primary_key=True, nullable=False))
 
 class Place(BaseModel, Base):
     """ A place to stay """
@@ -19,3 +25,34 @@ class Place(BaseModel, Base):
     latitude = Column(Float)
     longitude = Column(Float)
     amenity_ids = []
+
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        reviews = relationship("Review", cascade='all, delete, delete-orphan', backref="place")
+        amenities = relationship("Amenity", secondary=place_amenity, viewonly=False, back_populates="place_amenities")
+    else:
+        @property
+        def reviews(self):
+            """Returns reviews.id list"""
+            args = models.storage.all()
+            lista = []
+            res = []
+            for arg in args:
+                review = arg.replace('.', ' ')
+                review = shlex.split(review)
+                if (review[0] == 'Review'):
+                    lista.append(args[arg])
+            for elem in lista:
+                if (elem.place_id == self.id):
+                    result.append(elem)
+            return (result)
+
+        @property
+        def amenities(self):
+            """ Returns amenity ids' list """
+            return self.amenity_ids
+
+        @amenities.setter
+        def amenities(self, obj=None):
+            """ Adds amenity identifiers to the attribute."""
+            if type(obj) is Amenity and obj.id not in self.amenity_ids:
+                self.amenity_ids.append(obj.id)
